f10b39db23e56f6870a481a6df798aa4ca6643a4
Paul Narup <paul.narup@onesixsolutions.com>
2026-05-20 15:26:22 -0500

    cleanup

--- macros/relations/cortex_agent/drop_agent.sql
+++ macros/relations/cortex_agent/drop_agent.sql
@@ -1,6 +1,5 @@
--- Copyright 2025 OneSix Solutions
--- SPDX-License-Identifier: Apache-2.0
-
+-- Generates DROP AGENT IF EXISTS DDL for a Snowflake Cortex Agent.
+-- Called by dbt internals when a cortex_agent model is removed or replaced.
 {% macro snowflake__get_drop_cortex_agent_sql(relation) %}
 
   drop agent if exists
